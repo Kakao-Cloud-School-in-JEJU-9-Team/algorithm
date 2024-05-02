0879ffe3bf7cb1a62fd5ae33367a3352ac7f732d
SeungBeom Yu <74458310+sngbmYu@users.noreply.github.com>
2024-05-02 18:13:39 +0900

    kevin: 알고리즘 4주차 코드 2

---
+++ 2024/05/02/kevin.yu/2156번: 포도주 시식/Baekjoon$2156.java
@@ -0,0 +1,42 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon$2156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] triangle = new int[n][];
+        int[][] dp = new int[n][];
+        for (int i = 0; i < n; i++) {
+            triangle[i] = new int[i + 1];
+            dp[i] = new int[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+            }
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[n - 1][i]);
+        }
+        System.out.println(max);
+    }
+}
